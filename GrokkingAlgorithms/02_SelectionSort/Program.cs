16db53dac8fb971898512ddfdb9d14bf69df5e1d
M.Mahmoud <mohamed987456mm20@gmail.com>
2025-11-06 19:36:04 +0200

    add chatper two [selection sort]

---
+++ GrokkingAlgorithms/02_SelectionSort/Program.cs
@@ -0,0 +1,29 @@
+﻿
+// selection sort code 
+int[] arr = [2, 1, 5, 3, 4];
+
+var result = SelectionSort(arr);
+
+Print(result);
+
+static int[] SelectionSort(int[] arr)
+{
+    for (var i = 0; i < arr.Length - 1; i++)
+    {
+        int minIndex = i;
+        for (var j = i + 1; j < arr.Length; j++)
+        {
+            if (arr[j] < arr[minIndex])
+                minIndex = j;
+        }
+        if (minIndex != i)
+            (arr[i], arr[minIndex]) = (arr[minIndex], arr[i]);
+    }
+    return arr;
+}
+
+static void Print(int[] arr)
+{
+    foreach (var n in arr)
+        System.Console.WriteLine(n);
+}
